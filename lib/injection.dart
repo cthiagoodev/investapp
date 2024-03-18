dda6b2b97688c62c3136fe41008523aa4dcac58c
cthiagoodev <thiagosousadev@gmail.com>
2024-03-18 07:05:16 -0300

    alteracoes

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,18 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:investapp/modules/app/app.dart';
-import 'package:investapp/modules/home/home.dart';
-import 'package:investapp/modules/quote/quote.dart';
 import 'package:investapp/shared/shared.dart';
 
 final getIt = GetIt.I;
 
 void initInjection() {
-  ///Services
   getIt.registerSingleton<HttpClient>(DioService(Dio()));
-
-  ///Controllers
   getIt.registerSingleton<AppViewModel>(AppViewModel());
-  getIt.registerLazySingleton<QuoteController>(() => QuoteController());
-  getIt.registerLazySingleton<QuoteDetailController>(() => QuoteDetailController());
 }
