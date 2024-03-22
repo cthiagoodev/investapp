151670421ac2c10c74411da1ae706358ecfcbe18
cthiagoodev <thiagosousadev@gmail.com>
2024-03-22 01:40:42 -0300

    regsiter bloc

--- lib/modules/auth/bloc/register/register_bloc.dart
+++ lib/modules/auth/bloc/register/register_bloc.dart
@@ -1,2 +1,8 @@
+import 'package:bloc/bloc.dart';
+
 part 'register_state.dart';
 part 'register_event.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  RegisterBloc() : super(RegisterInitialState());
+}
